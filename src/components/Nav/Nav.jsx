b70c3dcf6e9a8f97964a44f279c605bd31e30fcb
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-05-20 15:42:52 -0600

    Nav Links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-
+import styles from "./Nav.module.css"
 
 export default function Nav(){
     return (
-        <nav>
-            <ul>
-                <li>
-               <link to="/">HomePage</link>
-               <link to="/calendar">Calendar</link>
-               <link to="/signin">Sign In</link>
-               <link to="/signup">Sign Up</link>
-                </li>
-            </ul>
-        </nav>
+        <nav className={styles.mainNav}>
+        <ul>
+          <li>
+            <Link to="/">HomePage</Link>
+          </li>
+          <li>
+            <Link to="/calendar">Calendar</Link>
+          </li>
+        </ul>
+      </nav>
     );
 }
