fc2ceb875074d42a17e981442e252b8565075572
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-05-20 13:28:29 -0600

    Added Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Calendar from './pages/Calendar'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
+import Nav from './components/Nav/Nav'
+
 
 function App() {
  
 
   return (
     <>
+    <Nav />
        <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/calendar" element={<Calendar />} />
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Nav(){
     return (
-        <nav className={StyleSheet.mainNav}>
+        <nav>
             <ul>
                 <li>
                <link to="/">HomePage</link>
