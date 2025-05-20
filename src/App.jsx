35451aa5ac7f72a475972ceaa9001978f3f3ce99
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-05-19 21:35:03 -0600

    Pages and Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
+import Calendar from './pages/Calendar'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
  
 
   return (
     <>
-    
+       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<Calendar />} />
+        <Route path="/dashboard" element={<SignIn />} />
+        <Route path="/create" element={<SignUp />} />
+      </Routes>
     </>
   )
 }
